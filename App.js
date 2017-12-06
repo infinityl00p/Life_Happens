dceeb7146738c2ead3807897bda56082a8e29ebc
james gill <infinityl00p>
2017-12-05 18:52:25 -0800

    visual countdown list

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import Header from './src/containers/Header'
+import { View } from 'react-native';
+import Header from './src/containers/Header';
+import CountdownList from './src/containers/CountdownList';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <View>
         <Header headerText={'Life Happens'} />
+        <CountdownList />
       </View>
     );
   }
 }
+
+export default App;
